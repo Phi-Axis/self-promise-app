d5f379f4d689a2bff0cf13a19412c117ef42aece
Phi-Axis <sunny.natura@gmail.com>
2026-03-27 18:53:59 +0900

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import "../global.css";
 import { Stack } from "expo-router";
 import { PromiseProvider } from "../lib/promise-context";
+import { TRPCProvider } from "../lib/trpc";
 
 export default function Layout() {
   return (
-    <PromiseProvider>
-      <Stack />
-    </PromiseProvider>
+    <TRPCProvider>
+      <PromiseProvider>
+        <Stack />
+      </PromiseProvider>
+    </TRPCProvider>
   );
 }
