cab39ca35764086f26661c2d6d5e19b554928266
Phi-Axis <sunny.natura@gmail.com>
2026-03-27 21:53:03 +0900

    Update history.tsx

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, FlatList, TouchableOpacity, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { usePromise } from "../lib/promise-context";
+import { usePromise } from "../../lib/promise-context";
 import { useColors } from "../hooks/use-colors";
 
 interface PromiseItem {
