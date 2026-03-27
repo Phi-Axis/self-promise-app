37e353dfa273892e563565c729667c3f1877be51
Phi-Axis <sunny.natura@gmail.com>
2026-03-27 11:19:34 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ const tailwindColors = Object.fromEntries(
 module.exports = {
   darkMode: "class",
   // Scan all component and app files for Tailwind classes
-  content: ["./app/**/*.{js,ts,tsx}", "./components/**/*.{js,ts,tsx}", "./lib/**/*.{js,ts,tsx}", "./hooks/**/*.{js,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./lib/**/*.{js,jsx,ts,tsx}", "./**/*.{js,jsx,ts,tsx}"//],
 
   presets: [require("nativewind/preset")],
   theme: {
