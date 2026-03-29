04fdaad0b4dd226652cdf1185d17044d32a1d749
Phi-Axis <sunny.natura@gmail.com>
2026-03-29 23:05:07 +0900

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,1 @@
-import { View, Text } from "react-native";
-
-export default function Page() {
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white text-2xl">TEST</Text>
-    </View>
-  );
-}
+export { default } from "../promise-input";
