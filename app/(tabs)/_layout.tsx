ac05d38595187a41c9fa1b2beb972b652b3dd13e
Phi-Axis <sunny.natura@gmail.com>
2026-04-02 15:19:38 +0900

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,10 @@ import { Tabs } from "expo-router";
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="home" options={{ title: "Home" }} />
       <Tabs.Screen name="history" options={{ title: "履歴" }} />
       <Tabs.Screen name="settings" options={{ title: "設定" }} />
-      <Tabs.Screen name="home" options={{ href: null }} />
+      <Tabs.Screen name="index" options={{ href: null }} />
     </Tabs>
   );
 }
