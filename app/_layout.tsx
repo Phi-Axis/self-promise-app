b9962cd1446c14d55ecf95b5275284216c96b28b
Phi-Axis <sunny.natura@gmail.com>
2026-04-02 14:01:15 +0900

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <TRPCProvider>
       <PromiseProvider>
-        <Stack />
+        <Stack screenOptions={{ headerShown: false }} />
       </PromiseProvider>
     </TRPCProvider>
   );
