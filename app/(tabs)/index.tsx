0c02d6a41ae959b2aa94fc1bd87346381526980b
Phi-Axis <sunny.natura@gmail.com>
2026-04-12 17:28:10 +0900

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,4 @@
 import { Redirect } from "expo-router";
-import { usePromise } from "../../lib/promise-context";
-import { ActivityIndicator, View } from "react-native";
-
-export default function Index() {
-  const { promise, isLoading } = usePromise();
-
-  if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-
-  import { Redirect } from "expo-router";
 
 export default function Index() {
   return <Redirect href="/home" />;
