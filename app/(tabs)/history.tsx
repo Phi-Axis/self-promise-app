54f49bf281ecc4f6059420d8f15f48133362626d
Phi-Axis <sunny.natura@gmail.com>
2026-04-13 19:31:18 +0900

    Update history.tsx

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -124,7 +124,6 @@ export default function HistoryScreen() {
           renderItem={renderItem}
           showsVerticalScrollIndicator={false}
         />
-      </View>
     </SafeAreaView>
   );
 }
