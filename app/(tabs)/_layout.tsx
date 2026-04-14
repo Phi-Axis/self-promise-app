92cb3a515f5d13f8214d90e385a70631ca74f349
Phi-Axis <sunny.natura@gmail.com>
2026-04-14 14:00:49 +0900

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
           title: "履歴",
           tabBarIcon: ({ focused }) => (
             <Ionicons
-              name="ribbon"
+              name="folder-outline"
               size={28}
               color={focused ? "#a99278" : "#c9c0b4"}
             />
